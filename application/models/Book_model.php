4bc09fde8f0f2e0d262f4db01bf5bf0999ea8508
R13Sergey <R13sergey@gmail.com>
2020-05-12 22:48:57 +0300

    Сделано хранение книг в БД

--- application/models/Book_model.php
+++ application/models/Book_model.php
@@ -7,16 +7,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Book_model extends CI_Model {
 
-	/**
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    /**
 	 * Загрузка списка книг
 	 */
 	public function loadList()
 	{
-		// todo реализовать получение списка книг из БД
-		return array(
-			array('book_id' => 1, 'book_name' => 'Евгений Онегин', 'author_name' => 'Пушкин А.С.', 'book_year' => 1833),
-			array('book_id' => 2, 'book_name' => 'Война и мир', 'author_name' => 'Толстой Л.Н.', 'book_year' => 1869),
-			array('book_id' => 3, 'book_name' => 'Анна Каренина', 'author_name' => 'Толстой Л.Н.', 'book_year' => 1877)
-		);
+        return $this->db->query('SELECT * FROM books')->result_array();
 	}
 }
